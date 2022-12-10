64ca12ad2c52daa3f8ddb9ad3707aad85ad3ffd4
Jefferson Lima <oliveiralima.jefferson@gmail.com>
2022-12-10 14:38:40 +0000

    Creation of the package model and the class User.java

---
+++ EirVidApp/src/model/User.java
@@ -0,0 +1,82 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author olive
+ */
+public class User {
+    
+    private int id;
+    private String name;
+    private String surname;
+    private int YearOfBirth;
+    private int cardNumber;
+    private String email;
+    private String password;
+
+    
+    
+     public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    } 
+    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public int getYearOfBirth() {
+        return YearOfBirth;
+    }
+
+    public void setYearOfBirth(int YearOfBirth) {
+        this.YearOfBirth = YearOfBirth;
+    }
+
+    public int getCardNumber() {
+        return cardNumber;
+    }
+
+    public void setCardNumber(int cardNumber) {
+        this.cardNumber = cardNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+
+  
+}
